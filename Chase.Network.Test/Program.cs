f8fc8badce2463514afbc1a1454f3290d6798900
Drew Chase <dcmanproductions@gmail.com>
2023-07-27 15:35:14 -0400

    added test code

--- Chase.Network.Test/Program.cs
+++ Chase.Network.Test/Program.cs
@@ -1,17 +1,28 @@
 ﻿// LFInteractive LLC. 2021-2024﻿ LFInteractive LLC. 2021-2024﻿ LFInteractive LLC. 2021-2024﻿
-// LFInteractive LLC. - All Rights Reserved
+// LFInteractive LLC. 2021-2024﻿ LFInteractive LLC. 2021-2024﻿ LFInteractive LLC. - All Rights Reserved
 using Chase.Networking;
-using Newtonsoft.Json;
+using Chase.Networking.Event;
+using Newtonsoft.Json.Linq;
 
 namespace Chase.Network.Test;
 
 internal class Program
 {
-    private static void Main()
+    private static async Task Main()
     {
         using NetworkClient client = new();
-        Newtonsoft.Json.Linq.JObject? obj = client.GetAsJson("https://api.modrinth.com/v2/search?query=The Warp Mod&limit=2&index=relevance&offset=0&facets=[[\"versions:1.19.4\"],[\"project_type:mod\"],[\"categories:fabric\"]]").Result;
-        Console.WriteLine(JsonConvert.SerializeObject(obj));
-        Console.Read();
+        using HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, "https://api.example.com");
+        JObject? jObject = await client.GetAsJson(request);
+        JArray? jArray = await client.GetAsJsonArray(request);
+
+        DownloadProgressEvent progressEvent = (s, e) =>
+        {
+            Console.WriteLine($"Download Progress {e.Percentage:P2}%");
+            Console.WriteLine($"Download Speed {e.BytesPerSecond}bps");
+            Console.WriteLine($"Downloaded {e.BytesDownloaded}bytes / {e.TotalBytesToReceive}bytes");
+            Console.WriteLine($"Remaining {e.BytesRemaining}bytes");
+        };
+
+        await client.DownloadFileAsync("https://example.com/download.zip", "/path/to/download.zip", progressEvent);
     }
 }
