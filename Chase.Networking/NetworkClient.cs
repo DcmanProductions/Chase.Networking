681211edb3eacb7f3ecbe5945f2fce54636fdf90
Drew Chase <dcmanproductions@gmail.com>
2023-09-13 23:26:15 -0400

    updated header

--- Chase.Networking/NetworkClient.cs
+++ Chase.Networking/NetworkClient.cs
@@ -1,8 +1,9 @@
 ﻿/*
-    FFNodes - LFInteractive LLC. 2021-2024
-    FFNodes is a client/server solution for batch processing ffmpeg operations from multiple systems across the internet.
-    Licensed under GPL-3.0
-    https://www.gnu.org/licenses/gpl-3.0.en.html#license-text
+Chase.Networking LFInteractive LLC. 2021-2024﻿
+a simple networking library
+https://github.com/dcmanProductions/Chase.Networking
+Licensed under GNU General Public License v3.0
+https://www.gnu.org/licenses/gpl-3.0.en.html
 */
 
 using Chase.Networking.Event;
